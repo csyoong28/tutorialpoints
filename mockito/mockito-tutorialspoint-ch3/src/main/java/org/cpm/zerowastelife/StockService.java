e1b6d006c24a42cec08d84c66e64a9709631396f
sy.chia <sy.chia@speed.com.my>
2019-03-28 15:42:56 +0800

    delete

--- mockito/mockito-tutorialspoint-ch3/src/main/java/org/cpm/zerowastelife/StockService.java
+++
@@ -1,8 +0,0 @@
-package org.cpm.zerowastelife;
-
-public interface StockService {
-
-	//IMPORTANT!
-	//we don't plan to implement this method, we try to use mockito to mock this method behavior
-	public double getPrice(Stock stock);
-}
